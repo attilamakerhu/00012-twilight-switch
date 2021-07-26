f8a5f43aa82cb3660f4e57dd93c9f12d2da34bfb
OolongJohnson <83778844+OolongJohnson@users.noreply.github.com>
2021-07-26 11:46:21 +0200

    code corrected

--- main.ts
+++ main.ts
@@ -1,6 +1,10 @@
-input.onButtonPressed(Button.A, function () {
-    music.playTone(262, 1)
-})
-input.onButtonPressed(Button.B, function () {
-    music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once)
+let adjusted_light_level = 0
+basic.forever(function () {
+    adjusted_light_level = input.lightLevel() - 30
+    basic.showNumber(adjusted_light_level)
+    if (adjusted_light_level > 150) {
+        pins.digitalWritePin(DigitalPin.P0, 0)
+    } else {
+        pins.digitalWritePin(DigitalPin.P0, 1)
+    }
 })
